14d28b4e0543658ece22c4e4025384d082c10600
ian <ptp_ian@photons.com.tw>
2024-05-28 14:57:05 +0800

    add greedy snake console

--- src/utils/greedySnake/snake.js
+++ src/utils/greedySnake/snake.js
@@ -6,15 +6,16 @@ export default class Snake {
     this.body = []
     this.maxLength = 5
     this.head = new Vector()
-    this.speed = new Vector(1, 0)
+    this.nextHead = new Vector(1, 0)
     this.direction = 'Right'
   }
 
   // 控制蛇移動
   update() {
     this.body.push(this.head)
-    const newHead = this.head.add(this.speed)
+    const newHead = this.head.add(this.nextHead)
     this.head = newHead
+
     while (this.body.length > this.maxLength) {
       this.body.shift()
     }
@@ -23,26 +24,40 @@ export default class Snake {
   // 控制蛇的方向
   setDirection(dir) {
     let target
-    if (dir === 'Up') {
-      target= new Vector(0, -1)
-    } else if (dir === 'Down') {
-      target= new Vector(0, 1)
-    } else if (dir === 'Left') {
-      target= new Vector(-1, 0)
-    } else if (dir === 'Right') {
-      target= new Vector(1, 0)
+    switch (dir) {
+      case 'Up': target = new Vector(0, -1); break
+      case 'Down': target = new Vector(0, 1); break
+      case 'Left': target = new Vector(-1, 0); break
+      case 'Right': target = new Vector(1, 0); break
+      default:
+        return
     }
 
     // 假如target和原本方向及相反方向不同
-    if (!target.equal(this.speed) && !target.equal(this.speed.mul(-1))) {
-      this.speed = target
+    if (!target.equal(this.nextHead) && !target.equal(this.nextHead.mul(-1))) {
+      this.nextHead = target
     }
   }
 
-  // 確認蛇有沒有撞到邊界
+  // 確認蛇有沒有在遊戲範圍內
   checkBoundary(gameWidth) {
     const xRange = 0 <= this.head.x && this.head.x < gameWidth
     const yRange = 0 <= this.head.y && this.head.y < gameWidth
     return xRange && yRange
+  }
+
+  // 無邊界模式要穿越邊框
+  setNewHead(gameWidth) {
+    let x = this.head.x
+    let y = this.head.y
+    // 方向往上
+    if (this.nextHead.y === -1) y = gameWidth
+    // 方向往下
+    if (this.nextHead.y === 1) y = 0
+    // 方向往左
+    if (this.nextHead.x === -1) x = gameWidth
+    // 方向往右
+    if (this.nextHead.x === 1) x = 0
+    this.head = new Vector(x, y)
   }
 }
--- src/views/GreedySnake.vue
+++ src/views/GreedySnake.vue
@@ -1,10 +1,49 @@
 <template>
-  <div class="panel">
-    <h2>Score: 0</h2>
-    <p>*提示:吃到綠色方塊不會增加蛇移動的速度，但紅色方塊會</p>
-    <button @click="game.startGame()">Start Game</button>
+  <div :class="$style.main">
+    <div :class="$style.game">
+      <div :class="$style.panel">
+        <h2>Score: 0</h2>
+        <p>*提示:吃到綠色方塊不會增加蛇移動的速度，但紅色方塊會</p>
+        <button @click="game.startGame()">Start Game</button>
+      </div>
+      <canvas id="snakeCanvas"></canvas>
+    </div>
+    <div :class="$style.console">
+      <div :class="$style.row">
+        <div>無邊界模式（撞牆不會死）</div>
+        <input v-model="borderless" type="checkbox" />
+      </div>
+      <div :class="$style.row">
+        <div>靜音</div>
+        <input v-model="mute" type="checkbox" />
+      </div>
+      <div :class="$style.row">
+        <div>block寬度</div>
+        <input v-model="bw" />
+      </div>
+      <div :class="$style.row">
+        <div>block邊框</div>
+        <input v-model="bs" />
+      </div>
+      <div :class="$style.row">
+        <div>長寬各有幾個格子</div>
+        <input v-model="gameWidth" />
+      </div>
+      <!-- <div :class="$style.row">
+        <div>速度</div>
+        <input v-model="speed" />
+      </div> -->
+      <div :class="$style.row">
+        <div>基礎速度</div>
+        <input v-model="baseSpeed" />
+      </div>
+      <div :class="$style.row">
+        <div>食物數量</div>
+        <input v-model="foodCount" />
+      </div>
+      <IcBtn :class="$style.btn" @click="updateGameConfig"> 重新開始 </IcBtn>
+    </div>
   </div>
-  <canvas id="mycanvas"></canvas>
 </template>
 
 <script>
@@ -13,64 +52,110 @@ import Game from '@/utils/greedySnake/game.js'
 
 export default {
   setup() {
+    const bw = ref(12) // block寬度
+    const bs = ref(2) // block邊框
+    const gameWidth = ref(40) // 長寬各有幾個格子
+    const baseSpeed = ref(40) // 基礎速度
+    const foodCount = ref(20) // 食物數量
+    const borderless = ref(false) // 無邊界模式（撞牆不會死
+    const mute = ref(true) // 靜音
     const game = ref(null)
+
     return {
+      bw,
+      bs,
+      gameWidth,
+      baseSpeed,
+      foodCount,
+      borderless,
+      mute,
       game,
     }
   },
+  computed: {
+    gameConfig() {
+      return {
+        bw: this.bw,
+        bs: this.bs,
+        gameWidth: this.gameWidth,
+        baseSpeed: this.baseSpeed,
+        foodCount: this.foodCount,
+        targetPanel: `.${this.$style.panel}`,
+        borderless: this.borderless,
+        mute: this.mute,
+      }
+    }
+  },
   methods: {
+    updateGameConfig() {
+      this.game.setConfig(this.gameConfig)
+    },
+    createGame() {
+      this.game = new Game(this.gameConfig)
 
+      //按鍵監聽
+      window.addEventListener('keydown', (evt) => {
+        this.game.snake.setDirection(evt.key.replace('Arrow', ''))
+      })
+    }
   },
   beforeMount() {
   },
   mounted() {
-    const config = {
-      bw: 12, // block寬度
-      bs: 2, // block邊框
-      gameWidth: 40, // 長寬各有幾個格子
-      speed: 100,
-      foodCount: 20, // 食物數量
-      baseSpeed: 40, // 基礎速度
-    }
-    this.game = new Game(config)
-
-    //按鍵監聽
-    window.addEventListener('keydown', (evt) => {
-      this.game.snake.setDirection(evt.key.replace('Arrow', ''))
-    })
+    this.createGame()
   },
 }
 </script>
 
-<style lang="scss">
-canvas {
-  max-height: 100vh
-}
-  
-.panel {
-  position: absolute;
-  top: 50%;
-  left: 100px;
-  color: #fff;
+<style lang="scss" module>
+.main {
   display: flex;
-  justify-content: center;
   align-items: center;
-  flex-direction: column;
-  z-index: 10;
-  h2 {
-    margin-bottom: 0;
+  justify-content: center;
+  padding: 20px;
+  .game {
+    position: relative;
+    margin-right: 10px;
+    .panel {
+      position: absolute;
+      top: 50%;
+      left: 100px;
+      color: #fff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      z-index: 10;
+      h2, p {
+        margin-bottom: 8px;
+      }
+      button {
+        background-color: transparent;
+        border: 1px solid #fff;
+        border-radius: 20px;
+        padding: 5px 10px;
+        color: #fff;
+        cursor: pointer;
+        outline: none;
+        &:hover {
+          background-color: #fff;
+          color: black;
+        }
+      }
+    }
+    canvas {
+      max-height: 100vh
+    }
   }
-  button {
-    background-color: transparent;
-    border: 1px solid #fff;
-    border-radius: 20px;
-    padding: 5px 10px;
-    color: #fff;
-    cursor: pointer;
-    outline: none;
-    &:hover {
-      background-color: #fff;
-      color: black;
+  .console {
+    & > .row {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin-bottom: 10px;
+      & > div:first-child {
+        margin-right: 8px;
+      }
     }
   }
 }
