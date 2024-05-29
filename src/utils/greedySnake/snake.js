14cb84fa7362efd0afa37542e6e314003e327d68
ian <ptp_ian@photons.com.tw>
2024-05-29 11:04:23 +0800

    fix greedy snake pressing button quickly causes the reverse direction

--- src/utils/greedySnake/snake.js
+++ src/utils/greedySnake/snake.js
@@ -6,14 +6,16 @@ export default class Snake {
     this.body = []
     this.maxLength = initialLength
     this.head = new Vector()
-    this.nextHead = new Vector(1, 0)
-    this.direction = 'Right'
+    this.currentHeadDirection = new Vector(1, 0) // 目前的方向
+    this.newHeadDirection = new Vector(1, 0) // 最新要變的方向
   }
 
   // 控制蛇移動
   update() {
     this.body.push(this.head)
-    const newHead = this.head.add(this.nextHead)
+    // 更新後再變更，以免快速按鍵時會出現反方向的情況
+    const newHead = this.head.add(this.newHeadDirection)
+    this.currentHeadDirection = this.newHeadDirection
     this.head = newHead
 
     while (this.body.length > this.maxLength) {
@@ -54,8 +56,8 @@ export default class Snake {
     }
 
     // 假如target和原本方向及相反方向不同
-    if (!target.equal(this.nextHead) && !target.equal(this.nextHead.mul(-1))) {
-      this.nextHead = target
+    if (!target.equal(this.currentHeadDirection) && !target.equal(this.currentHeadDirection.mul(-1))) {
+      this.newHeadDirection = target
     }
   }
 
@@ -76,13 +78,13 @@ export default class Snake {
     let x = this.head.x
     let y = this.head.y
     // 方向往上
-    if (this.nextHead.y === -1) y = gameWidth
+    if (this.currentHeadDirection.y === -1) y = gameWidth
     // 方向往下
-    if (this.nextHead.y === 1) y = 0
+    if (this.currentHeadDirection.y === 1) y = 0
     // 方向往左
-    if (this.nextHead.x === -1) x = gameWidth
+    if (this.currentHeadDirection.x === -1) x = gameWidth
     // 方向往右
-    if (this.nextHead.x === 1) x = 0
+    if (this.currentHeadDirection.x === 1) x = 0
     this.head = new Vector(x, y)
   }
 }
